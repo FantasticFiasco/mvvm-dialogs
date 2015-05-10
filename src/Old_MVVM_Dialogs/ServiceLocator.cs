54adc4242d91c372fee1ea24ec400b16629cce46
FantasticFiasco <mattias.kindborg@gmail.com>
2015-05-10 22:25:25 +0200

    Moved 'ServiceLocator' to old project since it no longer is needed by the library.

--- src/Old_MVVM_Dialogs/ServiceLocator.cs
+++ src/Old_MVVM_Dialogs/ServiceLocator.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
-namespace MvvmDialogs
+namespace MVVM_Dialogs
 {
     /// <summary>
     /// A very simple service locator.
