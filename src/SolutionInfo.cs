e5bce6714756d1496dadfd177be5af7f96702ae2
FantasticFiasco <mattias.kindborg@gmail.com>
2015-05-10 07:18:34 +0200

    Added a solution information file.

---
+++ src/SolutionInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyCompany("Fantastic Fiasco")]
+[assembly: AssemblyProduct("MVVM Dialogs")]
+[assembly: AssemblyCopyright("Copyright © 2009-2015 Fantastic Fiasco")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyInformationalVersion("1.1.0")]
