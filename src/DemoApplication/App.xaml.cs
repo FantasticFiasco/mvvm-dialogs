1f2251f67c7f8bb031b3febe9604d4876b99433d
FantasticFiasco <mattias.kindborg@gmail.com>
2015-05-12 22:13:26 +0200

    Added missing file.

---
+++ src/DemoApplication/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.Windows;
+using DemoApplication.View;
+using DemoApplication.ViewModel;
+using MvvmDialogs;
+
+namespace DemoApplication
+{
+    public partial class App
+    {
+        private CompositionContainer container;
+
+        [Import]
+        private MainWindow view;
+
+        [Import]
+        private MainWindowViewModel viewModel;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            container = CreateContainer();
+            container.SatisfyImportsOnce(this);
+
+            view.DataContext = viewModel;
+            view.Show();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+
+            container.Dispose();
+        }
+
+        private static CompositionContainer CreateContainer()
+        {
+            var catalog = new AggregateCatalog();
+            catalog.Catalogs.Add(new AssemblyCatalog(typeof(App).Assembly));
+            catalog.Catalogs.Add(new AssemblyCatalog(typeof(DialogService).Assembly));
+
+            return new CompositionContainer(catalog);
+        }
+    }
+}
