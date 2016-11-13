a6d3532ec33dd4773161424581db53379053c8cf
Mattias Kindborg <mattias.kindborg@gmail.com>
2016-11-13 21:49:47 +0100

    Add new project
    This project will serve as the demo of how to use custom modal dialogs.

---
+++ src/NET/Demos/Demo.ModalCustomDialog/ViewModelLocator.cs
@@ -0,0 +1,24 @@
+using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+
+namespace Demo.ModalCustomDialog
+{
+    /// <summary>
+    /// This class contains static references to all the view models in the
+    /// application and provides an entry point for the bindings.
+    /// </summary>
+    public class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            SimpleIoc.Default.Register<MainWindowViewModel>();
+            SimpleIoc.Default.Register<AddTextDialogViewModel>();
+        }
+
+        public MainWindowViewModel MainWindow => ServiceLocator.Current.GetInstance<MainWindowViewModel>();
+
+        public AddTextDialogViewModel AddTextDialog => ServiceLocator.Current.GetInstance<AddTextDialogViewModel>();
+    }
+}
