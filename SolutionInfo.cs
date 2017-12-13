111926130bbcb0719cc47285b79311f7f8e2b052
FantasticFiasco <mattias.kindborg@gmail.com>
2017-12-13 23:09:51 +0100

    wip

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -3,7 +3,7 @@
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyCompany("Fantastic Fiasco")]
+[assembly: AssemblyCompany("FantasticFiasco")]
 [assembly: AssemblyProduct("MVVM Dialogs")]
 [assembly: AssemblyCopyright("Copyright © 2009-2016 Mattias Kindborg")]
 [assembly: AssemblyTrademark("")]
