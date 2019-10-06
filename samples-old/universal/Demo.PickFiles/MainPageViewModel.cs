85761ca88338196c9d0d0adb2075ff276a355b30
FantasticFiasco <mattias.kindborg@gmail.com>
2019-10-07 00:05:52 +0200

    chore: disable samples
    In preparation for .NET Core 3, lets disable the sample project by
    removing them from the solution.

---
+++ samples-old/universal/Demo.PickFiles/MainPageViewModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using MvvmDialogs;
+using MvvmDialogs.FrameworkPickers.FileOpen;
+using IOPath = System.IO.Path;
+
+namespace Demo.PickFiles
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        private readonly IDialogService dialogService;
+
+        private string singleFilePath;
+        private string multipleFilesPath;
+
+        public MainPageViewModel(IDialogService dialogService)
+        {
+            this.dialogService = dialogService;
+
+            PickSingleFileCommand = new RelayCommand(PickSingleFile);
+            PickMultipleFilesCommand = new RelayCommand(PickMultipleFiles);
+        }
+
+        public string SingleFilePath
+        {
+            get => singleFilePath;
+            private set { Set(() => SingleFilePath, ref singleFilePath, value); }
+        }
+
+        public string MultipleFilesPath
+        {
+            get => multipleFilesPath;
+            private set { Set(() => MultipleFilesPath, ref multipleFilesPath, value); }
+        }
+
+        public ICommand PickSingleFileCommand { get; }
+
+        public ICommand PickMultipleFilesCommand { get; }
+
+        private async void PickSingleFile()
+        {
+            var settings = new FileOpenPickerSettings
+            {
+                SuggestedStartLocation = PickerLocationId.DocumentsLibrary,
+                FileTypeFilter = new List<string> { ".txt" }
+            };
+
+            StorageFile storageFile = await dialogService.PickSingleFileAsync(settings);
+            if (storageFile != null)
+            {
+                SingleFilePath = storageFile.Path;
+            }
+        }
+
+        private async void PickMultipleFiles()
+        {
+            var settings = new FileOpenPickerSettings
+            {
+                SuggestedStartLocation = PickerLocationId.DocumentsLibrary,
+                FileTypeFilter = new List<string> { ".txt" }
+            };
+
+            IReadOnlyList<StorageFile> storageFiles = await dialogService.PickMultipleFilesAsync(settings);
+            if (storageFiles.Any())
+            {
+                MultipleFilesPath = string.Join(";", storageFiles.Select(storageFile => storageFile.Path));
+            }
+        }
+    }
+}
