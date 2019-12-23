ba4fe79e480783054c082f36d82be8d100126b29
FantasticFiasco <mattias.kindborg@gmail.com>
2019-12-23 22:05:10 +0100

    wip

--- test/net/FrameworkDialogs/FolderBrowser/FolderBrowserDialogSettingsTest.cs
+++ test/net/FrameworkDialogs/FolderBrowser/FolderBrowserDialogSettingsTest.cs
@@ -1,8 +1,6 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using System.Windows.Forms;
+using MvvmDialogs.FrameworkDialogs.Utils;
 using NUnit.Framework;
 
 namespace MvvmDialogs.FrameworkDialogs.FolderBrowser
@@ -10,33 +8,20 @@ namespace MvvmDialogs.FrameworkDialogs.FolderBrowser
     [TestFixture]
     public class FolderBrowserDialogSettingsTest
     {
-        private static readonly string[] ExcludedPropertyNames =
-        {
-            "Container",
-            "Site",
-            "Tag"
-        };
-
         [Test]
         public void NativeDialogSettingsParity()
         {
             // Arrange
             var folderBrowserDialogPropertyNames = string.Join(
                 ", ",
-                GetPropertyNames(typeof(FolderBrowserDialog)).Except(ExcludedPropertyNames));
+                DialogSettings.GetPropertyNames(typeof(FolderBrowserDialog)).Except(DialogSettings.ExcludedPropertyNames));
 
             var folderBrowserDialogSettingsPropertyNames = string.Join(
                 ", ",
-                GetPropertyNames(typeof(FolderBrowserDialogSettings)));
+                DialogSettings.GetPropertyNames(typeof(FolderBrowserDialogSettings)));
 
             // Assert
             Assert.That(folderBrowserDialogPropertyNames, Is.EqualTo(folderBrowserDialogSettingsPropertyNames));
         }
-
-        private static IEnumerable<string> GetPropertyNames(Type type) =>
-            type
-                .GetProperties(BindingFlags.Public | BindingFlags.Instance)
-                .OrderBy(p => p.Name)
-                .Select(p => p.Name);
     }
 }
---
+++ test/net/FrameworkDialogs/Utils/DialogSettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace MvvmDialogs.FrameworkDialogs.Utils
+{
+    public static class DialogSettings
+    {
+        public static readonly string[] ExcludedPropertyNames =
+        {
+            "Container",
+            "Site",
+            "Tag"
+        };
+
+        public static IEnumerable<string> GetPropertyNames(Type type) =>
+            type
+                .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                .OrderBy(p => p.Name)
+                .Select(p => p.Name);
+    }
+}
