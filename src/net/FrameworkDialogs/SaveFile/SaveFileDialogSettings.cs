3ba4cd01e798c0b3fb7ee1cb84fd561bfa2a8065
FantasticFiasco <mattias.kindborg@gmail.com>
2019-12-24 11:03:14 +0100

    wip

--- src/net/FrameworkDialogs/SaveFile/SaveFileDialogSettings.cs
+++ src/net/FrameworkDialogs/SaveFile/SaveFileDialogSettings.cs
@@ -11,12 +11,20 @@ namespace MvvmDialogs.FrameworkDialogs.SaveFile
         /// Gets or sets a value indicating whether the dialog box prompts the user for permission
         /// to create a file if the user specifies a file that does not exist.
         /// </summary>
+        /// <value>
+        /// <c>true</c> if dialog should prompt prior to saving to a filename that did not
+        /// previously exist; otherwise, <c>false</c>. The default is <c>false</c>.
+        /// </value>
         public bool CreatePrompt { get; set; }
 
         /// <summary>
-        /// Gets or sets a value indicating whether the <b>Save As</b> dialog box displays a
-        /// warning if the user specifies a file name that already exists.
+        /// Gets or sets a value indicating whether the dialog box displays a warning if the user
+        /// specifies the name of a file that already exists.
         /// </summary>
+        /// <value>
+        /// <c>true</c> if dialog should prompt prior to saving over a filename that previously
+        /// existed; otherwise, <c>false</c>. The default is <c>true</c>.
+        /// </value>
         public bool OverwritePrompt { get; set; }
     }
 }
