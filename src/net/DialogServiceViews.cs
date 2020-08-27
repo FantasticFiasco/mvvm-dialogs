d67c593dca9e097691e676634f148f3138d05b9f
Mattias Kindborg @FantasticFiasco <FantasticFiasco@users.noreply.github.com>
2020-08-27 23:30:00 +0200

    fix: memory leak (#124)
    Fix memory leak where Window.Closed events never where un-registered.
    
    Closes #122

--- src/net/DialogServiceViews.cs
+++ src/net/DialogServiceViews.cs
@@ -109,7 +109,10 @@ namespace MvvmDialogs
 
             PruneInternalViews();
 
-            // Register for owner window closing, since we then should unregister view reference
+            // Register for owner window closing to cleanup views connected to this window, but
+            // only register for the event once, thus the un-registration of any prior
+            // registrations.
+            owner.Closed -= OwnerClosed;
             owner.Closed += OwnerClosed;
 
             Logger.Write($"Register view {view.Id}");
