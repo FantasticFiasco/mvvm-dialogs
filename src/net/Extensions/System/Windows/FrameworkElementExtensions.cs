1d03603f5136f6935a6d8329c1776702173fbc04
FantasticFiasco <mattias.kindborg@gmail.com>
2020-10-03 00:05:04 +0200

    refactor: minor formatting

--- src/net/Extensions/System/Windows/FrameworkElementExtensions.cs
+++ src/net/Extensions/System/Windows/FrameworkElementExtensions.cs
@@ -13,6 +13,8 @@ namespace System.Windows
         /// The <see cref="FrameworkElement"/> to find the <see cref="Window"/> for.
         /// </param> 
         /// <returns>The owning <see cref="Window"/> if found; otherwise null.</returns>
-        internal static Window GetOwner(this FrameworkElement frameworkElement) => (frameworkElement as Window ?? Window.GetWindow(frameworkElement)) ?? throw new InvalidOperationException();
+        // TODO: Fix possible 'null' assignment to non-nullable entity by making the entity nullable
+        internal static Window GetOwner(this FrameworkElement frameworkElement) =>
+            frameworkElement as Window ?? Window.GetWindow(frameworkElement);
     }
 }
