c9104bd117ffeeadbb35081339ec7b668cd69be4
C. Augusto Proiete <augusto@proiete.com>
2020-11-16 20:16:59 -0400

    feat: Update net-core/Demo.CustomMessageBox to use Ookii.Dialogs

--- samples/net-core/Demo.CustomMessageBox/CustomMessageBox.cs
+++ samples/net-core/Demo.CustomMessageBox/CustomMessageBox.cs
@@ -1,16 +1,14 @@
 ﻿using System;
 using System.Windows;
 using MvvmDialogs.FrameworkDialogs.MessageBox;
+using Ookii.Dialogs.Wpf;
 
 namespace Demo.CustomMessageBox
 {
-    /// <remarks>
-    /// This sample differs from the .NET Framework equivalent. The reason for that is that the
-    /// dependency Ookii.Dialogs.Wpf currently doesn't support .NET Core 3.
-    /// </remarks>
     public class CustomMessageBox : IMessageBox
     {
         private readonly MessageBoxSettings settings;
+        private readonly TaskDialog messageBox;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="CustomMessageBox"/> class.
@@ -19,6 +17,15 @@ namespace Demo.CustomMessageBox
         public CustomMessageBox(MessageBoxSettings settings)
         {
             this.settings = settings ?? throw new ArgumentNullException(nameof(settings));
+
+            messageBox = new TaskDialog
+            {
+                Content = settings.MessageBoxText
+            };
+
+            SetUpTitle();
+            SetUpButtons();
+            SetUpIcon();
         }
 
         /// <summary>
@@ -35,13 +42,84 @@ namespace Demo.CustomMessageBox
         {
             if (owner == null) throw new ArgumentNullException(nameof(owner));
 
-            return MessageBox.Show(
-                owner,
-                settings.MessageBoxText,
-                settings.Caption,
-                settings.Button,
-                settings.Icon,
-                settings.DefaultResult);
+            var result = messageBox.ShowDialog(owner);
+            return ToMessageBoxResult(result);
+        }
+
+        private void SetUpTitle()
+        {
+            messageBox.WindowTitle = string.IsNullOrEmpty(settings.Caption) ?
+                " " :
+                settings.Caption;
+        }
+
+        private void SetUpButtons()
+        {
+            switch (settings.Button)
+            {
+                case MessageBoxButton.OKCancel:
+                    messageBox.Buttons.Add(new TaskDialogButton(ButtonType.Ok));
+                    messageBox.Buttons.Add(new TaskDialogButton(ButtonType.Cancel));
+                    break;
+
+                case MessageBoxButton.YesNo:
+                    messageBox.Buttons.Add(new TaskDialogButton(ButtonType.Yes));
+                    messageBox.Buttons.Add(new TaskDialogButton(ButtonType.No));
+                    break;
+
+                case MessageBoxButton.YesNoCancel:
+                    messageBox.Buttons.Add(new TaskDialogButton(ButtonType.Yes));
+                    messageBox.Buttons.Add(new TaskDialogButton(ButtonType.No));
+                    messageBox.Buttons.Add(new TaskDialogButton(ButtonType.Cancel));
+                    break;
+
+                default:
+                    messageBox.Buttons.Add(new TaskDialogButton(ButtonType.Ok));
+                    break;
+            }
+        }
+
+        private void SetUpIcon()
+        {
+            switch (settings.Icon)
+            {
+                case MessageBoxImage.Error:
+                    messageBox.MainIcon = TaskDialogIcon.Error;
+                    break;
+
+                case MessageBoxImage.Information:
+                    messageBox.MainIcon = TaskDialogIcon.Information;
+                    break;
+
+                case MessageBoxImage.Warning:
+                    messageBox.MainIcon = TaskDialogIcon.Warning;
+                    break;
+
+                default:
+                    messageBox.MainIcon = TaskDialogIcon.Custom;
+                    break;
+            }
+        }
+
+        private static MessageBoxResult ToMessageBoxResult(TaskDialogButton button)
+        {
+            switch (button.ButtonType)
+            {
+                case ButtonType.Cancel:
+                    return MessageBoxResult.Cancel;
+
+                case ButtonType.No:
+                    return MessageBoxResult.No;
+
+                case ButtonType.Ok:
+                    return MessageBoxResult.OK;
+
+                case ButtonType.Yes:
+                    return MessageBoxResult.Yes;
+
+                default:
+                    return MessageBoxResult.None;
+            }
         }
     }
 }
