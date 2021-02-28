07ad1ca1b0f7efded99c9d34e90790f62193c597
Mattias Kindborg <mattiask@axis.com>
2021-02-28 15:43:08 +0100

    wip

--- samples/net-core/Demo.CustomDialogTypeLocator/MyCustomDialogTypeLocator.cs
+++ samples/net-core/Demo.CustomDialogTypeLocator/MyCustomDialogTypeLocator.cs
@@ -4,6 +4,8 @@ using MvvmDialogs.DialogTypeLocators;
 
 namespace Demo.CustomDialogTypeLocator
 {
+    // This class is used as an example in the wiki. For more information see
+    // https://github.com/FantasticFiasco/mvvm-dialogs/wiki/Custom-dialog-type-locators.
     public class MyCustomDialogTypeLocator : IDialogTypeLocator
     {
         public Type Locate(INotifyPropertyChanged viewModel)
