84725d240c3bd355a1f37e003f867af07c52aec7
Mattias Kindborg <mattiask@axis.com>
2021-02-28 15:05:50 +0100

    wip

--- samples/net-core/Demo.CustomDialogTypeLocator/ComponentA/MyDialogVM.cs
+++ samples/net-core/Demo.CustomDialogTypeLocator/ComponentA/MyDialogVM.cs
@@ -2,7 +2,14 @@
 
 namespace Demo.CustomDialogTypeLocator.Core.ComponentA
 {
-    public class MyDialogVM : ViewModelBase
+    public class MyDialogVM : IModalDialogViewModel, ViewModelBase
     {
+        private bool? dialogResult;
+
+        public bool? DialogResult
+        {
+            get => dialogResult;
+            private set => Set(nameof(DialogResult), ref dialogResult, value);
+        }
     }
 }
